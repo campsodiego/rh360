13d12b0177df76f1d79b654984f041dbe8c80ec7
Diego Rodrigues Campos <tecnico.ptt@gmail.com>
2017-10-05 11:48:34 -0300

    fix bug ip

--- postagens/postagem2.php
+++ postagens/postagem2.php
@@ -6,7 +6,29 @@
 <?php
 
 function getUserIP() {
-    return $_SERVER['REMOTE_ADDR'];
+    // return $_SERVER['REMOTE_ADDR'];
+
+    if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+    	
+        return $_SERVER['HTTP_CLIENT_IP'];
+    } else if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+
+        return $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else if(isset($_SERVER['HTTP_X_FORWARDED'])) {
+
+        return $_SERVER['HTTP_X_FORWARDED'];
+    } else if(isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+
+        return $_SERVER['HTTP_FORWARDED_FOR'];
+    } else if(isset($_SERVER['HTTP_FORWARDED'])) {
+
+        return $_SERVER['HTTP_FORWARDED'];
+    } else if(isset($_SERVER['REMOTE_ADDR'])) {
+
+        return $_SERVER['REMOTE_ADDR'];
+    } else {
+        return 'UNKNOWN';
+    }
 }
 
 
